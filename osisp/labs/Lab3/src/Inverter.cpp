b679043f6ed0ddeae1714daf49e47dd2fceac8c8
vassyan <vasa060504@mail.ru>
2024-03-09 14:38:54 +0300

    Osisp Lab3 added

---
+++ osisp/labs/Lab3/src/Inverter.cpp
@@ -0,0 +1,11 @@
+void invertBytes(unsigned char* buffer, size_t bufferSize) {
+    size_t left = 0;
+    size_t right = bufferSize - 1;
+    while (left < right) {
+        unsigned char temp = buffer[left];
+        buffer[left] = buffer[right];
+        buffer[right] = temp;
+        left++;
+        right--;
+    }
+}
---
+++ osisp/labs/Lab3/src/Source.cpp
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <stdlib.h>
+#include "inverter.h"
+#define BUFFER_SIZE 1024 // Определяем размер буфера для чтения
+
+int main(int argc, char* argv[]) {
+    bool IsFileOutput = false;
+    char* fileName = NULL;
+    if (argc == 2)
+    {
+        fileName = argv[1];
+        IsFileOutput = true;
+    }
+
+    FILE* inputFile, * outputFile = NULL;
+    unsigned char buffer[BUFFER_SIZE];
+    size_t bytesRead;
+
+    inputFile = fopen("input.bin", "rb");
+    if (inputFile == NULL) {
+        perror("Error opening input file");
+        return 1;
+    }
+
+    if (IsFileOutput)
+    {
+        outputFile = fopen(fileName, "wb");
+
+        if (outputFile == NULL) {
+            perror("Error opening output file");
+            fclose(inputFile);
+            return 1;
+        }
+    }
+
+    // Читаем данные блоками и инвертируем порядок байтов
+    while ((bytesRead = fread(buffer, sizeof(unsigned char), BUFFER_SIZE, inputFile)) > 0) {
+        invertBytes(buffer, bytesRead);
+        if (IsFileOutput)
+            fwrite(buffer, sizeof(unsigned char), bytesRead, outputFile);
+        else
+            fwrite(buffer, sizeof(unsigned char), bytesRead, stdout);
+    }
+
+    if (IsFileOutput)
+        fclose(outputFile);
+    
+
+    fclose(inputFile);
+
+    return 0;
+}
