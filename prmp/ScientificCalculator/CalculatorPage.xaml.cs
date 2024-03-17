6747e76e34034d54bee3a8d19e416c293a936c5c
vassyan <vasa060504@mail.ru>
2024-03-17 16:12:23 +0300

    Lexer added to this repo

---
+++ prmp/ScientificCalculator/CalculatorPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MauiScientificCalculator.ViewModels;
+
+namespace ScientificCalculator;
+
+public partial class CalculatorPage : ContentPage
+{
+    public CalculatorPage()
+    {
+        InitializeComponent();
+
+        //Initialize the view model
+        this.BindingContext = new CalculatorPageViewModel();
+    }
+}
