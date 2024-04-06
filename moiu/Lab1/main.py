0e0079e77c367015fcb2e6801fbaa2761afb5686
Vassiliy Bychko <vasa060504@mail.ru>
2024-04-06 20:15:13 +0300

    moiu lab4 done

--- moiu/Lab1/main.py
+++ moiu/Lab1/main.py
@@ -1,18 +1,18 @@
 import numpy as np
 from Lab3 import MySimplexMethodStartPhase
+from Lab4 import double_simplex_method
 
 
 
 if __name__ == "__main__":
-    A = np.array([[1, 1, 1, ],
-                  [2, 2, 2]])
+    A = np.array([[-2,-1,-4,1,0 ],
+                  [-2,-2,-2,0,1]])
     
-    c = np.array([1, 0, 0])
-    b = np.array([-1, 0])
+    c = np.array([-4,-3,-7,0,0])
+    b = np.array([-1, -3/2])
+    B = [4,5]
     
-    x, B, A, b = MySimplexMethodStartPhase(c,A,b)
     
-    print(x)
-    print(B)
-    print(A)
-    print(b)
+    result = double_simplex_method(c, A, b, B)
+    print("\n\n\n result")
+    print(result)
